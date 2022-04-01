7f0b4b5e19af7cd1bcd3f710f16b0ccd6ca75b37
janalg <gasparjanaina02@gmail.com>
2022-04-01 11:32:45 -0300

    up

---
+++ exercicios/src/exerciciosTestesEDebugs/SalarioAtualFuncionario.java
@@ -0,0 +1,12 @@
+package exerciciosTestesEDebugs;
+
+public class SalarioAtualFuncionario {
+	public static void main(String[] args) {
+		double valorInicial = 1000.0;
+		double primeiroAumento = valorInicial * 1.5 / 100;
+	
+		
+		
+		
+	}
+}
